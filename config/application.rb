6051b730a5b0b20a78d4130ebdb6dff4e5e89414
github username <github email address>
2019-09-11 17:27:51 +0100

    working api endpoint v1

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module ChelseaPlayerspage
   class Application < Rails::Application
+ 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -30,6 +31,6 @@ module ChelseaPlayerspage
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = true 
   end
 end
